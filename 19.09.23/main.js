067dd46d666ebf7830c1c6f2e35241c8ec766207
Mulko Artem <akm.dnepr@gmail.com>
2023-09-19 19:14:27 +0300

    19,09.23

---
+++ 19.09.23/main.js
@@ -0,0 +1,7 @@
+const name = "Артем";
+const surname = "Мулько";
+const age = 13;
+const hobby = "створювати Telegram-ботів";
+
+
+console.log(`Мене звати ${name} ${surname} мені ${age} років, у вільний час я полюбляю ${hobby}`);
